3876a3ee85a9b7add4a982fad03f6c40470a987f
LeeGlendenning <glen9190@mylaurier.ca>
2014-01-16 11:18:41 -0500

    Create Node.java
    A node class to aid with the linked stack class.

---
+++ Node.java
@@ -0,0 +1,75 @@
+/**
+ * @author Lee Glendenning
+ * id 120289190
+ * glen9190@mylaurier.ca
+ * @version Nov. 30, 2013
+ * 
+ */
+
+package glen9190;
+
+public class Node<A>
+{
+
+	
+    private A  element; //element of type Poly
+    private Node<A>  next; //points to next element
+
+    public Node() {
+    }
+    /**
+     * Constructor initializes Node
+     * 
+     * @param x
+     * @param node
+     */
+    public Node(A x, Node<A> node)
+    {
+        element = x;
+        next = node;
+    }
+    /**
+     * Set Element method, updates element of type Poly
+     * 
+     * @param el 
+     */
+	public void setElement(A el) {
+		element = el;
+	}
+	/**
+     * Set Node method, points at next node
+     * 
+     * @param n 
+     */
+	public void setNext(Node<A> n) {
+		next = n;
+	}
+	/**
+     * Get Element method, gets the current element of type Poly
+     * 
+     * 
+     * @return element 
+     */
+    public A getElement()
+    {
+        return element;
+    }
+    /**
+     * Get Next method, points to the next element
+     * 
+     * @return next element in node
+     */
+    public Node<A> getNext()
+    {
+        return next;
+    }
+    /**
+     * To String method 
+     * 
+     * @return string element in node
+     */
+    public String toString() {
+    	return "Node contains "+element;
+    }
+
+}
